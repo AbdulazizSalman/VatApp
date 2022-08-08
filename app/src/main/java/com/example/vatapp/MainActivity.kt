87703ea389665e45eabd6aded3c0762371cf38a9
Abdulaziz Al-halafi <109555706+AbdulazizSalman@users.noreply.github.com>
2022-08-08 12:39:43 +0300

    I activate the Calculate tax button and set the calculations to the inputs as well as tax Amount and Total Amount

--- app/src/main/java/com/example/vatapp/MainActivity.kt
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vatapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,6 +22,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateVat() {
+        val TextInput = binding.InputTotalCost.text.toString()
+        val cost = TextInput.toDouble()
+
+        val IdB = binding.RadioGroup.checkedRadioButtonId
+        val CheckRoundUp = binding.switchB.isChecked
+        val Per = when (IdB) {
+            R.id.ten_Button -> 0.10
+            R.id.fifteen_Button -> 0.15
+            else -> 0.20
+
+        }
+        val VAT = cost * Per
+        var TotalAmount = cost + VAT
+
+        if (CheckRoundUp) {
+            TotalAmount = kotlin.math.ceil(TotalAmount)
+        }
+        val TotalFormat = NumberFormat.getCurrencyInstance().format(TotalAmount)
+        val VatFormat = NumberFormat.getCurrencyInstance().format(VAT)
+
+        binding.TextCostTotal.text = getString(R.string.Total_Amount, TotalFormat)
+        binding.TextVat.text = getString(R.string.VAT_Amount, VatFormat)
+
     }
 
 
